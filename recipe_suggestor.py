d37b3791f441ed049aba96d448fb2714fafefb4d
sasi kumar <sasikumarmungara@gmail.com>
2022-09-19 14:33:30 +0530

    Automate recipe orders

--- recipe_suggestor.py
+++ recipe_suggestor.py
@@ -1,4 +1,5 @@
 import json
+import pandas as pd
 
 """
 
@@ -17,14 +18,14 @@ class CocktailSuggester:
         self.visualization = visualization
         self.ingredients = self.format_ingredients(ingredients)
         self.cocktails_to_ingredients, self.ingredients_to_cocktails, self.compatible_ingredients, self.recipe_ingredient_quantity = self.get_cocktail_lookups()
-        self.matched_recipe,self.partial_recipe = self.make_cocktail()
+        self.matched_recipe, self.partial_recipe = self.make_cocktail()
 
     def format_ingredients(self, ingredients):
         """format and normalize ingredients for better matching"""
         to_replace = {'cointreau': 'triple sec', '&': 'and'}
         updated_ingredients = []
         for i in ingredients:
-            i = i.lower().strip()
+            i = i.strip()
             if i in to_replace:
                 ingredient = to_replace[i]
             else:
@@ -33,20 +34,31 @@ class CocktailSuggester:
         return updated_ingredients
 
     def get_cocktail_lookups(self):
-        with open('recipes.txt', 'r') as read:
-            data = read.read().splitlines()
+        meals = pd.read_excel("recipes.xlsx", "Ingredients_List")
+        ingredients = pd.read_excel("recipes.xlsx", "Shelf_life and Price prediction")[
+            ["Ingredients_Id", "Ingredients"]]
+        meal_ingred = pd.merge(meals, ingredients, how="left", left_on="Ingredients_id", right_on="Ingredients_Id")[
+            ["meal_id", "Ingredients", "Amount(g)"]]
+
         cocktails_ingredients = {}
+        recipe_ingredients = {}
         ingredients_to_cocktails = {}
         # compatible ingredients are any ingredients that are found in a recipe together
         compatible_ingredients = {}
         recipe_ingredient_quantity = {}
-        for row in data:
-            cocktail_recipe = row.split(',')
-            cocktail_name = cocktail_recipe[0]
-            recipe_ingredient_quantity[cocktail_name] = set(
-                cocktail_recipe[1:])
-            ingredients = [cocktail_recipe[i].split(" ", 1)[1] for i in range(1, len(cocktail_recipe))]
-            cocktails_ingredients[cocktail_name] = set(ingredients)
+        for ind, row in meal_ingred.iterrows():
+            cocktail_name = str(row["meal_id"])
+            if row['meal_id'] not in recipe_ingredients.keys():
+                recipe_ingredients[row['meal_id']] = set([str(row["Ingredients"])])
+                recipe_ingredient_quantity[row['meal_id']] = set([str(row["Amount(g)"]) + "g " + row["Ingredients"]])
+            else:
+                recipe_ingredients[row['meal_id']] = recipe_ingredients[row['meal_id']].union(
+                    set([str(row["Ingredients"])]))
+                recipe_ingredient_quantity[row['meal_id']] = recipe_ingredient_quantity[row['meal_id']].union(
+                    set([str(row["Amount(g)"]) + "g " + row["Ingredients"]]))
+        for row in recipe_ingredients.items():
+            ingredients = row[1]
+            cocktail_name = row[0]
             for count, ingredient in enumerate(ingredients):
                 if ingredient not in ingredients_to_cocktails:
                     ingredients_to_cocktails[ingredient] = set([cocktail_name])
@@ -58,8 +70,7 @@ class CocktailSuggester:
                 else:
                     compatible_ingredients[ingredient].update(
                         [i for i in ingredients if i != ingredient])
-        return cocktails_ingredients, ingredients_to_cocktails, compatible_ingredients, recipe_ingredient_quantity
-
+        return recipe_ingredients, ingredients_to_cocktails, compatible_ingredients, recipe_ingredient_quantity
 
     def make_cocktail(self):
         """
@@ -89,29 +100,42 @@ class CocktailSuggester:
                         partial_recipe[j] = missing
             else:
                 print(i + " " + "not found in recipe list")
-        return matched_recipe,partial_recipe
-    def order_possible(self,ingredients,stock,recipes):
-        result={}
+        return matched_recipe, partial_recipe
+
+    def order_possible(self, ingredients, stock, recipes):
+        result = {}
         for recipe in recipes:
-            max_orders=999999
+            max_orders = 999999
             for ingr in self.cocktails_to_ingredients[recipe]:
                 if ingr in ingredients:
                     for k in self.recipe_ingredient_quantity[recipe]:
                         if ingr in k:
-                            ingredient_quantity=int(stock[ingredients.index(ingr)][:-1])//int(k.split(" ",1)[0][:-1])
-                            if max_orders>ingredient_quantity:
-                                max_orders=ingredient_quantity
+                            ingredient_quantity = int(stock[ingredients.index(ingr)][:-1]) // int(
+                                k.split(" ", 1)[0][:-1])
+                            if max_orders > ingredient_quantity:
+                                max_orders = ingredient_quantity
                             break
-            result[recipe]=max_orders
+            result[recipe] = max_orders
         print(result)
-
-
+def stock_left():
+    current_stock = pd.read_excel("inventory.xlsx", "current_stock")
+    ingredients = pd.read_excel("recipes.xlsx", "Shelf_life and Price prediction")[["Ingredients_Id", "Ingredients"]]
+    ingred_stock_left_in_inventory=pd.merge(current_stock,ingredients,how='left',left_on='Items',right_on='Ingredients_Id')[['Ingredients','Amount(g)','Shelf life(days)']]
+    ingredients=[]
+    stock=[]
+    for index,ingred in ingred_stock_left_in_inventory.iterrows():
+        if ingred['Shelf life(days)']<=5:
+            ingredients.append(ingred['Ingredients'])
+            stock.append(str(ingred['Amount(g)'])+'g')
+    return ingredients,stock
 if __name__ == '__main__':
-    input_ingredients = ["rice","chicken",'cheese','tomato']
+    input_ingredients,stock = stock_left()
+    print(input_ingredients)
+    print(stock)
     cocktails = CocktailSuggester(input_ingredients, ratio=0, visualization=True)
     for option in cocktails.matched_recipe:
         print(option)
-    cocktails.order_possible(input_ingredients, ['2100g', '300g','100g','100g'], cocktails.matched_recipe)
+    cocktails.order_possible(input_ingredients,stock, cocktails.matched_recipe)
     # for item in cocktails.order_possible(input_ingredients,['140g','200g'],cocktails.matched_recipe):
     #     print(item)
     for partial_cocktail, missing_ingredients in cocktails.partial_recipe.items():
